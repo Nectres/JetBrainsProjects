c21739668415177a9bf2c18f9a73043c8cba75c5
Keerthi Vasan <sakeerthi23@gmail.com>
2021-12-30 10:44:44 +0530

    Knight's tour stage 2: custom board dimensions

--- python/Knight's Tour puzzle.py
+++ python/Knight's Tour puzzle.py
@@ -1,36 +1,67 @@
 class KnightTour:
-    def __init__(self, x, y):
-        # Generating a 8x8 board with empty ('_') places
-        self.board = [['_' for _ in range(8)] for _ in range(8)]
-        # Placing the knight in the board at the given position
-        self.board[x][y] = 'X'
+    def __init__(self):
+        cols, rows = self.get_dimensions(
+            "Enter your board dimensions: ", lambda x: x > 0, decrement=False)
+        self.cell_size = len(str(rows * cols))
+        self.border = ' ' + '-' * (cols * (self.cell_size + 1) + 3)
+        self.rows = rows
+        self.empty_cell = self.cell_size * '_'
+        self.knight = "X".rjust(self.cell_size, ' ')
+        self.board = [
+            [self.empty_cell for _ in range(cols)] for _ in range(rows)]
+        self.cols = cols
+        self.get_knight_pos()
 
     def print_board(self):
-        print(' -------------------')
-        for i in range(7, -1, -1):
+        print(self.border)
+        for i in range(self.rows-1, -1, -1):
             print(f'{i+1}| ', end='')
-            for j in range(8):
+            for j in range(self.cols):
                 print(self.board[i][j], end=' ')
             print('|')
-        print(' -------------------')
-        print('   1 2 3 4 5 6 7 8 ')
+        print(self.border)
+        print('   ', end='')
+        for i in range(1, self.cols+1):
+            print(str(i).rjust(self.cell_size,' '), end=' ')
+
+    def get_knight_pos(self):
+        col, row = self.get_dimensions("Enter the knight's starting position: ", lambda x: 0 <= x <= self.cols-1, lambda y: 0 <= y <= self.rows-1)
+        self.board[row][col] = self.knight
+
+    def get_dimensions(self, prompt: str, check_x, check_y=None, decrement=True):
+        valid = False
+        if not check_y:
+            check_y = check_x
+        while not valid:
+            string = input(prompt)
+
+            try:
+                coordinates = [int(x) for x in string.split()]
+            except ValueError:
+                print("Invalid dimensions!")
+                continue
+
+            if len(coordinates) != 2:
+                print("Invalid dimensions!")
+                continue
+
+            x, y = coordinates
+
+            if decrement:
+                x -= 1
+                y -= 1
+
+            if not check_x(x) or not check_y(y):
+                print("Invalid dimensions!")
+                continue
+
+            # If all checks have passed, the position is valid and we can break the loop
+            valid = True
+        return x, y
 
 
 def main():
-    string = input("Enter the knight's starting position: ")
-    try:
-        coordinates = [int(x) - 1 for x in string.split()]
-    except ValueError:
-        print("Invalid dimensions!")
-        return
-    if len(coordinates) != 2:
-        print("Invalid dimensions!")
-        return
-    y, x = coordinates
-    if not 0 <= x <= 7 or not 0 <= y <= 7:
-        print("Invalid dimensions!")
-        return
-    game = KnightTour(x, y)
+    game = KnightTour()
     game.print_board()
 
 
